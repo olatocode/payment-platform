e33d0ad9b7ad3cd2f8d44119eaf4483c2c16d033
olatocode <awosolat@gmail.com>
2025-07-10 16:34:39 +0100

    server credentials update

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Paystack configuration
